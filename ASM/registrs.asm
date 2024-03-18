b8184757317223e0f8165b13c826704940d7cce3
Rev0x1337 <163394237+Rev0x1337@users.noreply.github.com>
2024-03-18 03:45:29 +0000

    Update and rename registrs.txt to registrs.asm

---
+++ ASM/registrs.asm
@@ -0,0 +1,26 @@
+A - Accumulator
+B - Base Register
+C - Count Register
+D - Data Register
+-------------------------
+
+AH, AL    ;| 8b 1\2 16b
+BH, BL    ;|
+CH, CL    ;|
+DH, DL    ;|
+
+AX        ;| 16b 1\2 32b
+BX        ;|
+CX        ;|
+DX        ;|
+
+EAX       ;| 32b 1\2 64b
+EBX       ;|
+ECX       ;|
+EDX       ;|
+
+RAX       ;| 64b 1\2 128b
+RBX       ;|
+RCX       ;|
+RDX       ;|
+
