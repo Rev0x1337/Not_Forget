b5361312d04cf9a7323c7cb0a8ef09950d076b3b
Rev0x1337 <163394237+Rev0x1337@users.noreply.github.com>
2024-03-18 04:21:17 +0000

    Update registrs.asm

--- ASM/registrs.asm
+++ ASM/registrs.asm
@@ -23,4 +23,46 @@ RAX       ;| 64b 1\2 128b
 RBX       ;|
 RCX       ;|
 RDX       ;|
+**************************
 
+Segment Register (arch x86)
+
+CS - Code Segment
+DS - Data Segment
+SS - Stack Segment
+ES, FS, GS - Extra Segment
+**************************
+ 
+Registers - pointers
+
+SP - Stack Pointer
+SPL   ; 8b
+SP    ; 16b
+ESP   ; 32b
+RSP   ; 64b
+
+BP - start of stack frame
+BPL   ; 8b
+BP    ; 16b
+EBP   ; 32b
+RBP   ; 64b
+
+IP - Instruction Pointer
+IP    ; 16b
+EIP   ; 32b
+RIP   ; 64b
+**************************
+
+Index registers
+
+SI - Sourse Index
+SIL   ; 8b
+SI    ; 16b
+ESI   ; 32b
+RSI   ; 64b
+
+DI - Destination Index
+DIL   ; 8b
+DI    ; 16b
+EDI   ; 32b
+RDI   ; 64b
