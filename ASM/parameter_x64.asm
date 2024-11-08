dea24153909f94fdd7b249d0e417696668217ec1
Rev0x1337 <163394237+Rev0x1337@users.noreply.github.com>
2024-11-08 12:14:14 +0300

    Update parameter_x64.asm

--- ASM/parameter_x64.asm
+++ ASM/parameter_x64.asm
@@ -1,1 +1,25 @@
-;
+; 4 parameters
+
+sub rsp, 40 ; -stack
+
+mov r9, param_4
+mov r8, param_3
+mov rdx, param_2
+mov rcx, param_1
+call f_api64
+
+add rsp 40 ; +stack
+________________________
+
+; 5 parameters
+
+sub rsp, 40 ; -stack
+
+mov qword ptr [rsp+32], param_5
+mov r9, param_4
+mov r8, param_3
+mov rdx, param_2
+mov rcx, param_1
+call f_api64
+
+add rsp, 40 ; +stack
